87dd189b8a19943277b8b5d0cf08b53468da7c4c
bazalaki <40613708+bazalaki@users.noreply.github.com>
2018-12-11 16:01:19 -0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+Your name is: <?php echo $_POST['name']; ?><br />
+Your e-mail: <?php echo $_POST['email']; ?><br />
+Comments:<br />
+<?php echo $_POST['message']; ?>
+</body>
+</html>
